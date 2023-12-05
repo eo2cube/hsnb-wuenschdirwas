c68e9d8945607e403060b9a424b5181b4e25ede9
Christoph Friedrich <christoph.friedrich@uni-wuerzburg.de>
2023-12-05 16:00:00 +0100

    Implement NDVI
    Based on https://openlayers.org/en/latest/examples/cog-math.html

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
       <ol-source-osm />
     </ol-tile-layer>
 
-    <ol-webgl-tile-layer :style="trueColor" ref="layer">
+    <ol-webgl-tile-layer :style="ndvi" ref="layer">
       <ol-source-geo-tiff ref="source"
         :sources="[
           {
@@ -43,6 +43,10 @@
           },
           {
             url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/33/U/UV/2023/7/S2A_33UUV_20230715_0_L2A/B02.tif',
+            max: 10000,
+          },
+          {
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/33/U/UV/2023/7/S2A_33UUV_20230715_0_L2A/B08.tif',
             max: 10000,
           },
         ]"
@@ -84,6 +88,42 @@ const trueColor = ref({
   color: ["array", red, green, blue, 1],
   gamma: 1.1,
 });
+
+// band numbers according to the order in the `sources` array above (1-based)
+const NDVI_FORMULA = ['/', ['-', ['band', 4], ['band', 1]], ['+', ['band', 4], ['band', 1]]];
+
+// color ramp for vegetation index
+const colorstops = [
+  -0.2,  [191, 191, 191],
+  -0.1,  [219, 219, 219],
+  0,     [255, 255, 224],
+  0.025, [255, 250, 204],
+  0.05,  [237, 232, 181],
+  0.075, [222, 217, 156],
+  0.1,   [204, 199, 130],
+  0.125, [189, 184, 107],
+  0.15,  [176, 194, 97],
+  0.175, [163, 204, 89],
+  0.2,   [145, 191, 82],
+  0.25,  [128, 179, 71],
+  0.3,   [112, 163, 64],
+  0.35,  [97, 150, 54],
+  0.4,   [79, 138, 46],
+  0.45,  [64, 125, 36],
+  0.5,   [48, 110, 28],
+  0.55,  [33, 97, 18],
+  0.6,   [15, 84, 10],
+  0.65,  [0, 69, 0]
+];
+
+const ndvi = {
+  color: [
+    'interpolate',
+    ['linear'],
+    NDVI_FORMULA,
+    ...colorstops
+  ],
+};
 
 const cloudcover = ref(30);
 const bbox = ref('');
